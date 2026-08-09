556eeb0cfc71cbe92b11efd065079fa100f361dc
ShivankXD <shivankprabhudessai@gmail.com>
2026-08-09 10:24:29 +0530

    Bump to 1.6.3

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1393,7 +1393,7 @@ QMenuBar::item:pressed {
         QMessageBox::about(win, "About NativeOffice",
             "<h3>NativeOffice Impress</h3>"
             "<p>A high-performance, cross-platform presentation tool.</p>"
-            "<p>Version 1.6.2</p>"
+            "<p>Version 1.6.3</p>"
             "<p>NativeOffice is your go to OfficeSuite!</p>");
     });
 
@@ -1704,7 +1704,7 @@ int main(int argc, char* argv[]) {
     // IMPORTANT: Set org/app before the first QSettings use (including
     // RecentFilesManager singleton construction triggered below)
     app.setApplicationName("NativeOffice");
-    app.setApplicationVersion("1.6.2");
+    app.setApplicationVersion("1.6.3");
     app.setOrganizationName("NativeOffice");
     app.setOrganizationDomain("nativeoffice.app");
 
